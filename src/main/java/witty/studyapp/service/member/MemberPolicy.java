968e1458a7133993e8e9f128b968885056797242
kangyunsik <re12io@ajou.ac.kr>
2021-09-30 18:19:36 +0900

    Subdivision of MemberPolicyV1

--- src/main/java/witty/studyapp/service/member/MemberPolicy.java
+++ src/main/java/witty/studyapp/service/member/MemberPolicy.java
@@ -3,5 +3,9 @@ package witty.studyapp.service.member;
 import witty.studyapp.dto.member.MemberRegisterDTO;
 
 public interface MemberPolicy {
-    boolean verifyRegister(MemberRegisterDTO memberRegisterDTO);
+    boolean verifyRegisterDTO(MemberRegisterDTO memberRegisterDTO);
+    boolean isValidIdent(String ident);
+    boolean isValidPassword(String password);
+    boolean isValidEmail(String email);
+    boolean isValidName(String name);
 }
--- src/main/java/witty/studyapp/service/member/impl/MemberPolicyV1.java
+++ src/main/java/witty/studyapp/service/member/impl/MemberPolicyV1.java
@@ -19,7 +19,7 @@ public class MemberPolicyV1 implements MemberPolicy {
     private final int MIN_NAME_LENGTH = 4;
 
     @Override
-    public boolean verifyRegister(MemberRegisterDTO memberRegisterDTO) {
+    public boolean verifyRegisterDTO(MemberRegisterDTO memberRegisterDTO) {
         return (
                 isValidIdent(memberRegisterDTO.getIdent()) &&
                         isValidPassword(memberRegisterDTO.getPassword()) &&
@@ -32,7 +32,8 @@ public class MemberPolicyV1 implements MemberPolicy {
      * ID 정책 : 영문자, 숫자만 조합 가능
      * MIN_ID_LENGTH <= {length} <= MAX_ID_LENGTH
      */
-    private boolean isValidIdent(String ident) {
+    @Override
+    public boolean isValidIdent(String ident) {
         return isValid("[0-9a-zA-Z]*$", ident, MIN_ID_LENGTH, MAX_ID_LENGTH);
     }
 
@@ -41,7 +42,8 @@ public class MemberPolicyV1 implements MemberPolicy {
      * 허용 특수문자 : ! @ # $ % & * ~ , .
      * MIN_ID_LENGTH <= {length} <= MAX_ID_LENGTH
      */
-    private boolean isValidPassword(String password) {
+    @Override
+    public boolean isValidPassword(String password) {
         return isValid("[0-9a-zA-Z!@#$%&*~,.]*$", password, MIN_ID_LENGTH, MAX_ID_LENGTH);
     }
 
@@ -49,7 +51,8 @@ public class MemberPolicyV1 implements MemberPolicy {
      * Email 정책 : '@' 포함 && 아이디 조건 && 이메일 조건
      * 이메일 조건 : Patters Class 참조
      */
-    private boolean isValidEmail(String email) {
+    @Override
+    public boolean isValidEmail(String email) {
         boolean isEmail;
         if (email == null) return false;
         isEmail = Pattern.matches(
@@ -62,7 +65,8 @@ public class MemberPolicyV1 implements MemberPolicy {
      * Name 정책 : Only 영문
      * MIN_ID_LENGTH <= {length} <= MAX_ID_LENGTH
      */
-    private boolean isValidName(String name) {
+    @Override
+    public boolean isValidName(String name) {
         return isValid("[a-zA-Z]*$",name,MIN_NAME_LENGTH,MAX_NAME_LENGTH);
     }
 
