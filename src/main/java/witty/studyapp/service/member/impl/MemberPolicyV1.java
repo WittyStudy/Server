e6ac2a2e7e27a83ea5ee4247965d2ac8c7feab64
kangyunsik <re12io@ajou.ac.kr>
2021-09-30 18:04:13 +0900

    PolicyV1 edited

--- src/main/java/witty/studyapp/service/member/impl/MemberPolicyV1.java
+++ src/main/java/witty/studyapp/service/member/impl/MemberPolicyV1.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 import witty.studyapp.dto.member.MemberRegisterDTO;
 import witty.studyapp.service.member.MemberPolicy;
 
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
 @Slf4j
 @Component
 public class MemberPolicyV1 implements MemberPolicy {
@@ -12,8 +15,8 @@ public class MemberPolicyV1 implements MemberPolicy {
     private final int MAX_ID_LENGTH = 16;
     private final int MIN_ID_LENGTH = 4;
 
-    private final int MAX_EMAIL_LENGTH = 64;
-    private final int MIN_EMAIL_LENGTH = 4;
+    private final int MAX_NAME_LENGTH = 16;
+    private final int MIN_NAME_LENGTH = 4;
 
     @Override
     public boolean verifyRegister(MemberRegisterDTO memberRegisterDTO) {
@@ -30,7 +33,7 @@ public class MemberPolicyV1 implements MemberPolicy {
      * MIN_ID_LENGTH <= {length} <= MAX_ID_LENGTH
      */
     private boolean isValidIdent(String ident) {
-        return isValid("[^0-9a-zA-Z]", ident, MIN_ID_LENGTH, MAX_ID_LENGTH);
+        return isValid("[0-9a-zA-Z]*$", ident, MIN_ID_LENGTH, MAX_ID_LENGTH);
     }
 
     /**
@@ -39,34 +42,32 @@ public class MemberPolicyV1 implements MemberPolicy {
      * MIN_ID_LENGTH <= {length} <= MAX_ID_LENGTH
      */
     private boolean isValidPassword(String password) {
-        return isValid("[^0-9a-zA-Z!@#$%&*~,.]", password, MIN_ID_LENGTH, MAX_ID_LENGTH);
+        return isValid("[0-9a-zA-Z!@#$%&*~,.]*$", password, MIN_ID_LENGTH, MAX_ID_LENGTH);
     }
 
     /**
      * Email 정책 : '@' 포함 && 아이디 조건 && 이메일 조건
-     * 아이디 조건 : MIN_ID_LENGTH <= {length} <= MAX_ID_LENGTH
-     * 이메일 조건 : MIN_EMAIL_LENGTH <= {length} <= MAX_EMAIL_LENGTH
+     * 이메일 조건 : Patters Class 참조
      */
     private boolean isValidEmail(String email) {
-        if (email.split("@").length != 2)
-            return false;
-        return isValid("", email.split("@")[0], MIN_ID_LENGTH, MAX_ID_LENGTH)
-                && isValid("", email.split("@")[1], MIN_EMAIL_LENGTH, MAX_EMAIL_LENGTH)
-                && isValid("[^!]", email.split("@")[1], 1, 1);
+        boolean isEmail;
+        if (email == null) return false;
+        isEmail = Pattern.matches(
+                "[\\w~\\-.]+@[\\w~\\-]+(\\.[\\w~\\-]+)+",
+                email.trim());
+        return isEmail;
     }
 
     /**
-     * Name 정책 :
+     * Name 정책 : Only 영문
+     * MIN_ID_LENGTH <= {length} <= MAX_ID_LENGTH
      */
     private boolean isValidName(String name) {
-        return true;
+        return isValid("[a-zA-Z]*$",name,MIN_NAME_LENGTH,MAX_NAME_LENGTH);
     }
 
     private boolean isValid(String regex, String string, int min, int max) {
-        String modified = string.replaceAll(regex, "");
-        log.debug("string =      '{}'", string);
-        log.debug("modified  =   '{}'", modified);
-        return string.length() == modified.length() &&
+        return string.matches(regex) &&
                 string.length() >= min &&
                 string.length() <= max;
     }
--- src/test/java/witty/studyapp/service/member/impl/MemberPolicyV1Test.java
+++ src/test/java/witty/studyapp/service/member/impl/MemberPolicyV1Test.java
@@ -1,5 +1,7 @@
 package witty.studyapp.service.member.impl;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import witty.studyapp.dto.member.MemberRegisterDTO;
 
@@ -7,55 +9,73 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberPolicyV1Test {
 
-    @Test
-    void validationTest1(){
-        MemberRegisterDTO dto = new MemberRegisterDTO();
-        dto.setIdent("Ident");
-        dto.setName("");
-        dto.setPassword("");
-        dto.setEmail("");
+    MemberRegisterDTO dto;
 
+    @BeforeEach
+    void beforeEach(){
+        dto = new MemberRegisterDTO();
+        dto.setIdent("Ident");
+        dto.setName("MyName");
+        dto.setPassword("password");
+        dto.setEmail("minsoo@hello.com");
+    }
+
+    @Test
+    @DisplayName("테스트 기본 설정 확인")
+    void identTest0(){
+        MemberPolicyV1 memberPolicyV1 = new MemberPolicyV1();
+        assertThat(memberPolicyV1.verifyRegister(dto)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("정상 아이디 확인")
+    void identTest1(){
+        dto.setIdent("Ident");
 
         MemberPolicyV1 memberPolicyV1 = new MemberPolicyV1();
         assertThat(memberPolicyV1.verifyRegister(dto)).isEqualTo(true);
     }
 
     @Test
-    void validationTest2(){
-        MemberRegisterDTO dto = new MemberRegisterDTO();
+    @DisplayName("공백 포함 비정상 아이디 확인")
+    void identTest2(){
         dto.setIdent("Ide  ");
-        dto.setName("");
-        dto.setPassword("");
-        dto.setEmail("");
-
 
         MemberPolicyV1 memberPolicyV1 = new MemberPolicyV1();
         assertThat(memberPolicyV1.verifyRegister(dto)).isEqualTo(false);
     }
 
     @Test
-    void validationTest3(){
-        MemberRegisterDTO dto = new MemberRegisterDTO();
-        dto.setIdent("Ide   ");
-        dto.setName("");
-        dto.setPassword("");
-        dto.setEmail("");
-
+    @DisplayName("탭 포함 비정상 아이디 확인")
+    void identTest3(){
+        dto.setIdent("Ide\t");
 
         MemberPolicyV1 memberPolicyV1 = new MemberPolicyV1();
         assertThat(memberPolicyV1.verifyRegister(dto)).isEqualTo(false);
     }
 
     @Test
-    void validationTest4(){
-        MemberRegisterDTO dto = new MemberRegisterDTO();
+    @DisplayName("중간 공백 포함 비정상 아이디 확인")
+    void identTest4(){
         dto.setIdent("Ide d");
-        dto.setName("");
-        dto.setPassword("");
-        dto.setEmail("");
-
 
         MemberPolicyV1 memberPolicyV1 = new MemberPolicyV1();
         assertThat(memberPolicyV1.verifyRegister(dto)).isEqualTo(false);
     }
+
+    @Test
+    @DisplayName("영문 한글 혼합 비정상 이름 확인")
+    void identTest5(){
+        dto.setName("김minz");
+
+        MemberPolicyV1 memberPolicyV1 = new MemberPolicyV1();
+        assertThat(memberPolicyV1.verifyRegister(dto)).isEqualTo(false);
+    }
+
+    @Test
+    void testTest(){
+        String string = "Hello2";
+        System.out.println(string.matches("[a-zA-Z]*$"));
+    }
+
 }
