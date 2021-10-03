7b6d33d9f580feede172d32d1e2fdce1a4e9c146
Kangyunsik <re12io@ajou.ac.kr>
2021-10-03 18:28:24 +0900

    Jasypt encryption config

---
+++ src/main/java/witty/studyapp/config/JasyptConfig.java
@@ -0,0 +1,21 @@
+package witty.studyapp.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean("commonEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("!encryptor_password%");
+        encryptor.setPoolSize(1);
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
