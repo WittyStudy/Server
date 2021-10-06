4253da3d5dfbc731a9cb6758ffd3576e4822bf25
kangyunsik <re12io@ajou.ac.kr>
2021-10-06 23:45:06 +0900

    Setting external logging file's name & simplifying properties

--- src/main/java/witty/studyapp/StudyAppApplication.java
+++ src/main/java/witty/studyapp/StudyAppApplication.java
@@ -1,7 +1,5 @@
 package witty.studyapp;
 
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
