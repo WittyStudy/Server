ec08a9fbcb71e58aacf901cac724ca2d261f84ed
kangyunsik <re12io@ajou.ac.kr>
2021-12-10 23:38:37 +0900

    Separating constants from member policyV1

---
+++ src/main/java/witty/studyapp/constant/member/MemberConstant.java
@@ -0,0 +1,12 @@
+package witty.studyapp.constant.member;
+
+public interface MemberConstant {
+    int MAX_NAME_LENGTH = 16;
+    int MIN_NAME_LENGTH = 4;
+
+    int MAX_EMAIL_LENGTH = 16;
+    int MIN_EMAIL_LENGTH = 4;
+
+    int MAX_PASSWORD_LENGTH = 16;
+    int MIN_PASSWORD_LENGTH = 8;
+}
