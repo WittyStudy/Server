58c7f484d06aff8f4c2131a8b12dfc08bf743820
Kangyunsik <re12io@ajou.ac.kr>
2021-12-11 18:21:34 +0900

    Custom Exception Define

--- src/main/java/witty/studyapp/execption/NoAuthorizationException.java
+++ src/main/java/witty/studyapp/execption/NoAuthorizationException.java
@@ -3,9 +3,9 @@ package witty.studyapp.execption;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class LoginArgumentException extends IllegalArgumentException{
-    public LoginArgumentException(String s) {
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class NoAuthorizationException extends SecurityException{
+    public NoAuthorizationException(String s) {
         super(s);
     }
 }
--- src/main/java/witty/studyapp/execption/NotLoginMemberException.java
+++ src/main/java/witty/studyapp/execption/NotLoginMemberException.java
@@ -3,9 +3,9 @@ package witty.studyapp.execption;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class LoginArgumentException extends IllegalArgumentException{
-    public LoginArgumentException(String s) {
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class NotLoginMemberException extends SecurityException{
+    public NotLoginMemberException(String s) {
         super(s);
     }
 }
