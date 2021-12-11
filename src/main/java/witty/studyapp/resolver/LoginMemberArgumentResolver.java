1915968731963dc086e5bac3176f0786f2e31bd6
Kangyunsik <re12io@ajou.ac.kr>
2021-12-11 16:22:06 +0900

    Login Custom Annotation for login session handling

---
+++ src/main/java/witty/studyapp/resolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,34 @@
+package witty.studyapp.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import witty.studyapp.annotation.Login;
+import witty.studyapp.dto.member.MemberLoginDTO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static witty.studyapp.constant.session.SessionConstant.LOGIN_MEMBER;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasMemberType = MemberLoginDTO.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginAnnotation && hasMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return null;
+        }
+        return session.getAttribute(LOGIN_MEMBER);
+    }
+}
