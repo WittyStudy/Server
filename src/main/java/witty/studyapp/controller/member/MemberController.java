1766e8d6593f37e3819e030a8ea14f210012cae3
Kangyunsik <re12io@ajou.ac.kr>
2021-12-11 18:23:38 +0900

    API modify to (remove {memberId} and using session)

--- src/main/java/witty/studyapp/controller/member/MemberController.java
+++ src/main/java/witty/studyapp/controller/member/MemberController.java
@@ -5,10 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import witty.studyapp.annotation.Login;
 import witty.studyapp.dto.member.MemberLoginDTO;
 import witty.studyapp.dto.member.MemberRegisterDTO;
 import witty.studyapp.entity.Member;
 import witty.studyapp.execption.LoginArgumentException;
+import witty.studyapp.execption.NotLoginMemberException;
+import witty.studyapp.execption.RegisterArgumentException;
 import witty.studyapp.service.member.MemberService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +19,6 @@ import javax.servlet.http.HttpSession;
 import java.util.List;
 
 import static witty.studyapp.constant.session.SessionConstant.LOGIN_MEMBER;
-import static witty.studyapp.constant.session.SessionConstant.LOGIN_UUID;
 
 @RestController
 @Slf4j
@@ -37,7 +39,14 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public Long register(@RequestBody @Validated MemberRegisterDTO memberRegisterDTO) {
+    public Long register(@RequestBody @Validated MemberRegisterDTO memberRegisterDTO, BindingResult bindingResult) {
+        log.info("/register called.");
+
+        if(bindingResult.hasErrors()){
+            log.info("binding result error. [/members/register]");
+            throw new RegisterArgumentException("회원가입 형태가 올바르지 않습니다.");
+        }
+
         Member member = new Member();
         member.setName(memberRegisterDTO.getName());
         member.setPassword(memberRegisterDTO.getPassword());
@@ -48,9 +57,11 @@ public class MemberController {
     @PostMapping("/login")
     public Object login(@RequestBody @Validated MemberLoginDTO memberLoginDTO, BindingResult bindingResult,
                         HttpServletRequest request) {
+        log.info("/login called.");
+
         if (bindingResult.hasErrors()) {
-            log.info("binding result error. [{}]", request.getAttribute(LOGIN_UUID));
-            throw new LoginArgumentException("로그인 형태가 올바르지 않습니다");
+            log.info("binding result error. [/members/login]");
+            throw new LoginArgumentException("로그인 형태가 올바르지 않습니다.");
         }
 
         Member member = new Member();
@@ -61,21 +72,34 @@ public class MemberController {
             HttpSession session = request.getSession(true);
             session.setAttribute(LOGIN_MEMBER, m);
             return m;
-        }).orElseThrow(()-> new LoginArgumentException("이메일과 비밀번호가 올바르지 않습니다."));
+        }).orElseThrow(()-> {
+            log.info("login fail.");
+            return new LoginArgumentException("이메일과 비밀번호가 올바르지 않습니다.");
+        });
     }
 
-    @PatchMapping("/{memberId}/name")
-    public Long patchMemberName(@PathVariable long memberId, @RequestBody String name) {
-        return memberService.updateMemberName(memberId, name);
+    @PostMapping("/logout")
+    public String logout(@Login Member loginMember, HttpServletRequest request){
+        if(loginMember == null){
+            throw new NotLoginMemberException("세션이 유효하지 않습니다.");
+        }
+
+        request.getSession().setMaxInactiveInterval(0);
+        return "OK";
     }
 
-    @PatchMapping("/{memberId}/password")
-    public Long patchMemberPassword(@PathVariable long memberId, @RequestBody String password) {
-        return memberService.updateMemberPassword(memberId, password);
+    @PatchMapping("/name")
+    public Long patchMemberName(@Login Member loginMember, @RequestBody String name) {
+        return memberService.updateMemberName(loginMember.getId(), name);
     }
 
-    @DeleteMapping("/{memberId}")
-    public Long deleteMemberById(@PathVariable long memberId) {
-        return memberService.deleteMember(memberId);
+    @PatchMapping("/password")
+    public Long patchMemberPassword(@Login Member loginMember, @RequestBody String password) {
+        return memberService.updateMemberPassword(loginMember.getId(), password);
+    }
+
+    @DeleteMapping("/")
+    public Long deleteMemberById(@Login Member loginMember) {
+        return memberService.deleteMember(loginMember.getId());
     }
 }
