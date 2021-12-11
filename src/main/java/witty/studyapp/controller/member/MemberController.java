bb22c8fbae409470b3de0435cd84b6c903a414eb
Kangyunsik <re12io@ajou.ac.kr>
2021-12-11 16:24:21 +0900

    Member API refactor (for using session)

--- src/main/java/witty/studyapp/controller/member/MemberController.java
+++ src/main/java/witty/studyapp/controller/member/MemberController.java
@@ -2,13 +2,21 @@ package witty.studyapp.controller.member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import witty.studyapp.dto.member.MemberLoginDTO;
 import witty.studyapp.dto.member.MemberRegisterDTO;
 import witty.studyapp.entity.Member;
+import witty.studyapp.execption.LoginArgumentException;
 import witty.studyapp.service.member.MemberService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
+
+import static witty.studyapp.constant.session.SessionConstant.LOGIN_MEMBER;
+import static witty.studyapp.constant.session.SessionConstant.LOGIN_UUID;
 
 @RestController
 @Slf4j
@@ -20,15 +28,16 @@ public class MemberController {
 
     /**
      * TEST CODE
+     *
      * @return All members
      */
     @GetMapping
-    public List<Member> test(){
+    public List<Member> test() {
         return memberService.getAllMembers();
     }
 
-    @PostMapping
-    public Long register(@RequestBody @Validated MemberRegisterDTO memberRegisterDTO){
+    @PostMapping("/register")
+    public Long register(@RequestBody @Validated MemberRegisterDTO memberRegisterDTO) {
         Member member = new Member();
         member.setName(memberRegisterDTO.getName());
         member.setPassword(memberRegisterDTO.getPassword());
@@ -36,23 +45,37 @@ public class MemberController {
         return memberService.register(member);
     }
 
-    @PostMapping("/{memberId}/login")
-    public Object login(@PathVariable long memberId, @RequestBody Member member){
-        return memberService.login(memberId, member);
+    @PostMapping("/login")
+    public Object login(@RequestBody @Validated MemberLoginDTO memberLoginDTO, BindingResult bindingResult,
+                        HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            log.info("binding result error. [{}]", request.getAttribute(LOGIN_UUID));
+            throw new LoginArgumentException("로그인 형태가 올바르지 않습니다");
+        }
+
+        Member member = new Member();
+        member.setEmail(memberLoginDTO.getEmail());
+        member.setPassword(memberLoginDTO.getPassword());
+
+        return memberService.login(member).map((m) -> {
+            HttpSession session = request.getSession(true);
+            session.setAttribute(LOGIN_MEMBER, m);
+            return m;
+        }).orElseThrow(()-> new LoginArgumentException("이메일과 비밀번호가 올바르지 않습니다."));
     }
 
     @PatchMapping("/{memberId}/name")
-    public Long patchMemberName(@PathVariable long memberId, @RequestBody String name){
+    public Long patchMemberName(@PathVariable long memberId, @RequestBody String name) {
         return memberService.updateMemberName(memberId, name);
     }
 
     @PatchMapping("/{memberId}/password")
-    public Long patchMemberPassword(@PathVariable long memberId, @RequestBody String password){
-        return memberService.updateMemberPassword(memberId,password);
+    public Long patchMemberPassword(@PathVariable long memberId, @RequestBody String password) {
+        return memberService.updateMemberPassword(memberId, password);
     }
 
     @DeleteMapping("/{memberId}")
-    public Long deleteMemberById(@PathVariable long memberId){
+    public Long deleteMemberById(@PathVariable long memberId) {
         return memberService.deleteMember(memberId);
     }
 }
