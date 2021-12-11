f1693e8827ed7e824e54585605fa8892c2c66193
Kangyunsik <re12io@ajou.ac.kr>
2021-12-11 16:22:22 +0900

    Create LoginArgumentException

---
+++ src/main/java/witty/studyapp/execption/LoginArgumentException.java
@@ -0,0 +1,7 @@
+package witty.studyapp.execption;
+
+public class LoginArgumentException extends IllegalArgumentException{
+    public LoginArgumentException(String s) {
+        super(s);
+    }
+}
