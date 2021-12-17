00e40fed6e54b50f0ca4d3001b45681a32e6c4fa
kangyunsik <re12io@ajou.ac.kr>
2021-12-18 00:10:55 +0900

    Reflecting Constraints on Comments length

--- src/main/java/witty/studyapp/dto/comment/CommentCreateDTO.java
+++ src/main/java/witty/studyapp/dto/comment/CommentCreateDTO.java
@@ -1,9 +1,14 @@
 package witty.studyapp.dto.comment;
 
 import lombok.Data;
+import witty.studyapp.constant.comment.CommentConstant;
+
+import javax.validation.constraints.Size;
 
 @Data
 public class CommentCreateDTO {
     private Long boardId;
+
+    @Size(min = CommentConstant.MIN_CONTENT_LENGTH, max = CommentConstant.MAX_CONTENT_LENGTH)
     private String content;
 }
