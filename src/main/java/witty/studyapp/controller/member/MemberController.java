3f18e38102e60fda4054cbc66b91cdd79fa0b5c6
kangyunsik <re12io@ajou.ac.kr>
2021-12-17 14:19:00 +0900

    Divide MemberController -> (LoginController, RegisterController, MemberController)

--- src/main/java/witty/studyapp/controller/member/MemberController.java
+++ src/main/java/witty/studyapp/controller/member/MemberController.java
@@ -6,18 +6,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import witty.studyapp.annotation.Login;
-import witty.studyapp.dto.member.MemberLoginDTO;
 import witty.studyapp.dto.member.MemberRegisterDTO;
 import witty.studyapp.entity.Member;
-import witty.studyapp.execption.LoginArgumentException;
 import witty.studyapp.execption.RegisterArgumentException;
 import witty.studyapp.service.member.MemberService;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
-
-import static witty.studyapp.constant.session.SessionConstant.LOGIN_MEMBER;
 
 @RestController
 @Slf4j
@@ -35,52 +29,6 @@ public class MemberController {
     @GetMapping("/test")
     public List<Member> test() {
         return memberService.getAllMembers();
-    }
-
-    @PostMapping("/register")
-    public Long register(@RequestBody @Validated MemberRegisterDTO memberRegisterDTO, BindingResult bindingResult) {
-        log.info("/register called.");
-
-        if(bindingResult.hasErrors()){
-            log.info("binding result error. [/members/register]");
-            throw new RegisterArgumentException("회원가입 형태가 올바르지 않습니다.");
-        }
-
-        Member member = new Member();
-        member.setName(memberRegisterDTO.getName());
-        member.setPassword(memberRegisterDTO.getPassword());
-        member.setEmail(memberRegisterDTO.getEmail());
-        return memberService.register(member);
-    }
-
-    @PostMapping("/login")
-    public Object login(@RequestBody @Validated MemberLoginDTO memberLoginDTO, BindingResult bindingResult,
-                        HttpServletRequest request) {
-        log.info("/login called.");
-
-        if (bindingResult.hasErrors()) {
-            log.info("binding result error. [/members/login]");
-            throw new LoginArgumentException("로그인 형태가 올바르지 않습니다.");
-        }
-
-        Member member = new Member();
-        member.setEmail(memberLoginDTO.getEmail());
-        member.setPassword(memberLoginDTO.getPassword());
-
-        return memberService.login(member).map((m) -> {
-            HttpSession session = request.getSession(true);
-            session.setAttribute(LOGIN_MEMBER, m);
-            return m;
-        }).orElseThrow(()-> {
-            log.info("login fail.");
-            return new LoginArgumentException("이메일과 비밀번호가 올바르지 않습니다.");
-        });
-    }
-
-    @PostMapping("/logout")
-    public String logout(HttpServletRequest request){
-        request.getSession(false).invalidate();
-        return "OK";
     }
 
     @PatchMapping("/name")
---
+++ src/main/java/witty/studyapp/controller/member/RegisterController.java
@@ -0,0 +1,37 @@
+package witty.studyapp.controller.member;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import witty.studyapp.dto.member.MemberRegisterDTO;
+import witty.studyapp.entity.Member;
+import witty.studyapp.execption.RegisterArgumentException;
+import witty.studyapp.service.member.MemberService;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+public class RegisterController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/register")
+    public Long register(@RequestBody @Validated MemberRegisterDTO memberRegisterDTO, BindingResult bindingResult) {
+        log.info("/register called.");
+
+        if(bindingResult.hasErrors()){
+            log.info("binding result error. [/members/register]");
+            throw new RegisterArgumentException("회원가입 형태가 올바르지 않습니다.");
+        }
+
+        Member member = new Member();
+        member.setName(memberRegisterDTO.getName());
+        member.setPassword(memberRegisterDTO.getPassword());
+        member.setEmail(memberRegisterDTO.getEmail());
+        return memberService.register(member);
+    }
+}
